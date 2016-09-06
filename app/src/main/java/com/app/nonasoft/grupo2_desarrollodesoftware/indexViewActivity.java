2acc045486751cae27fe9d80145ee048786cd48a
adara denis <adaradenis@gmail.com>
2016-09-06 20:37:52 -0300

    + pantalla principal + funcionalidad btn comenzar + cambio de activity

---
+++ app/src/main/java/com/app/nonasoft/grupo2_desarrollodesoftware/indexViewActivity.java
@@ -0,0 +1,17 @@
+package com.app.nonasoft.grupo2_desarrollodesoftware;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by adara on 06/09/2016.
+ */
+public class IndexViewActivity extends Activity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index_view);
+
+    }
+}
